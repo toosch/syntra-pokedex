ab8470b708278a4b3d6c34e1dca9539dda251de1
toosch <toon.schuurmans@gmail.com>
2021-04-30 08:52:42 +0200

    better detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,6 +2,10 @@ import React from "react";
 import useSwr from "swr";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function Overview() {
   const pokemon = useParams().name;
@@ -12,10 +16,34 @@ function Overview() {
   );
   return (
     <>
-      <Link to="/">Back to homepage</Link>
-      {data && <p>{pokemon}</p>}
-      {data && <img src={data.sprites.front_default} alt={pokemon}></img>}
-      {error && <p>Oops, something went wrong.</p>}
+      <Container className="p-0">
+        <Link to="/">
+          <Button className="d-block mx-auto mb-4">Back to overview</Button>
+        </Link>
+        <Card>
+          {data && (
+            <>
+              <Card.Img
+                className="poke-img border shadow d-flex mx-auto my-3"
+                src={data.sprites.front_default}
+                alt={pokemon}
+              ></Card.Img>
+              <Card.Body className="text-secondary text-center">
+                {console.log(data)}
+                {data && <h3>{pokemon}</h3>}
+                <ListGroup className="">
+                  <ListGroup.Item>weight: {data.weight}</ListGroup.Item>
+                  <ListGroup.Item>height: {data.height}</ListGroup.Item>
+                </ListGroup>
+              </Card.Body>
+            </>
+          )}
+          {error && <p>Oops, something went wrong.</p>}
+        </Card>
+        <Link to="/">
+          <Button className="d-block mx-auto mt-4">Back to overview</Button>
+        </Link>
+      </Container>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
 function Header() {
   return (
-    <Container className="p-0 pt-4">
-      <img
+    <Container className="p-0 pt-4 px-3">
+      <Image
         src="https://fontmeme.com/permalink/210427/4a7fbbfb3ac7c7d406e6c0af1fc9df6b.png"
         alt="Syntra FSD"
-        className="d-block mx-auto"
+        className="d-block mx-auto img-fluid"
       />
-      <img
+      <Image
         src="https://fontmeme.com/permalink/210427/a5a91fee17e4c754d4fb741965382f04.png"
         alt="Pokédex"
-        className="d-block mx-auto mb-4"
+        className="d-block mx-auto mb-4 img-fluid"
       />
     </Container>
   );
